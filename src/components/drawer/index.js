cafb9c0928537d05f8c2aa89fc7300f412311b5c
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2023-05-14 16:43:07 -0500

    configure drawer and logo

---
+++ src/components/drawer/index.js
@@ -0,0 +1,55 @@
+import { Divider, Drawer, List, ListItemButton, ListItemText } from '@mui/material';
+import { styled } from '@mui/system';
+import CloseIcon from '@mui/icons-material/Close';
+import { lighten } from 'polished';
+import { useUIContext } from '../../context/ui';
+import { DrawerCloseButton } from '../../styles/appbar';
+import { Colors } from '../../theme/palette';
+
+const MiddleDivider = styled((props) => <Divider variant="middle" {...props} />)``;
+
+export default function AppDrawer() {
+  const { drawerOpen, setDrawerOpen } = useUIContext();
+  return (
+    <>
+      {drawerOpen && (
+        <DrawerCloseButton onClick={() => setDrawerOpen(false)}>
+          <CloseIcon
+            sx={{
+              fontSize: '2.5em',
+              color: lighten(0.09, Colors.secondary),
+            }}
+          />
+        </DrawerCloseButton>
+      )}
+      <Drawer open={drawerOpen}>
+        <List>
+          <ListItemButton>
+            <ListItemText>Home</ListItemText>
+          </ListItemButton>
+          <MiddleDivider />
+          <ListItemButton>
+            <ListItemText>Rates</ListItemText>
+          </ListItemButton>
+          <MiddleDivider />
+          <ListItemButton>
+            <ListItemText>About</ListItemText>
+          </ListItemButton>
+          <MiddleDivider />
+          <ListItemButton>
+            <ListItemText>Contact Us</ListItemText>
+          </ListItemButton>
+          <MiddleDivider />
+          <ListItemButton>
+            <ListItemText>Login</ListItemText>
+          </ListItemButton>
+          <MiddleDivider />
+          <ListItemButton>
+            <ListItemText>SignUp</ListItemText>
+          </ListItemButton>
+          <MiddleDivider />
+        </List>
+      </Drawer>
+    </>
+  );
+}
