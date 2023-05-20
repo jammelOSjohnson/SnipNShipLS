4a9bbcb84d8ff92581e600b022f60c09e0fe3eba
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2023-05-20 12:47:06 -0500

    added tiktok icon

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,11 +1,29 @@
-import { Typography, Grid, Box, List, ListItemText } from '@mui/material';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import { Typography, Grid, Box, List, ListItemText, Button, ListItemButton } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import { useNavigate } from 'react-router-dom';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TikTokIcon from '../tiktokicon';
 import { Colors } from '../../theme/palette';
 import { FooterTitle } from '../../styles/footer';
 
 export default function Footer() {
+  const navigate = useNavigate();
+  const handleSocial = function handleSocial(social) {
+    switch (social) {
+      case 'facebook':
+        window.open('https://www.facebook.com/profile.php?id=100092457001052');
+        break;
+      case 'instagram':
+        window.open('https://instagram.com/snipnship?igshid=NTc4MTIwNjQ2YQ==');
+        break;
+      case 'tiktok':
+        window.open('https://www.tiktok.com/@snipnship3?_t=8cLbin6qTA6&_r=1');
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
     <>
       <Box
@@ -32,54 +50,74 @@ export default function Footer() {
                 color: Colors.dove_gray,
               }}
             >
-              <FacebookIcon sx={{ mr: 1 }} />
-              <TwitterIcon sx={{ mr: 1 }} />
-              <InstagramIcon sx={{ mr: 1 }} />
+              <Button sx={{ color: 'inherit' }} onClick={() => handleSocial('facebook')}>
+                <FacebookIcon sx={{ mr: 1 }} />
+              </Button>
+              <Button sx={{ color: 'inherit' }} onClick={() => handleSocial('tiktok')}>
+                <TikTokIcon color={Colors.dove_gray} width="1.5em" height="1.5em" style={{ marginRight: 1 }} />
+              </Button>
+              <Button sx={{ color: 'inherit' }} onClick={() => handleSocial('instagram')}>
+                <InstagramIcon sx={{ mr: 1 }} />
+              </Button>
             </Box>
           </Grid>
           <Grid item md={6} lg={2}>
             <FooterTitle variant="body1">Information</FooterTitle>
             <List>
-              <ListItemText>
-                <Typography lineHeight={2} variant="capton2">
-                  About Us
-                </Typography>
-              </ListItemText>
-              <ListItemText>
-                <Typography lineHeight={2} variant="capton2">
-                  Our Rates
-                </Typography>
-              </ListItemText>
-              <ListItemText>
-                <Typography lineHeight={2} variant="capton2">
-                  Privacy &amp; Policy
-                </Typography>
-              </ListItemText>
-              <ListItemText>
-                <Typography lineHeight={2} variant="capton2">
-                  Terms &amp; Conditions
-                </Typography>
-              </ListItemText>
+              <ListItemButton>
+                <ListItemText>
+                  <Typography lineHeight={2} variant="capton2">
+                    About Us
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemText>
+                  <Typography lineHeight={2} variant="capton2">
+                    Our Rates
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemText>
+                  <Typography lineHeight={2} variant="capton2">
+                    Privacy &amp; Policy
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+              <ListItemButton>
+                <ListItemText>
+                  <Typography lineHeight={2} variant="capton2">
+                    Terms &amp; Conditions
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
             </List>
           </Grid>
           <Grid item md={6} lg={2}>
             <FooterTitle variant="body1">my account</FooterTitle>
             <List>
-              <ListItemText>
-                <Typography lineHeight={2} variant="capton2">
-                  Login
-                </Typography>
-              </ListItemText>
-              <ListItemText>
-                <Typography lineHeight={2} variant="capton2">
-                  SignUp
-                </Typography>
-              </ListItemText>
-              <ListItemText>
-                <Typography lineHeight={2} variant="capton2">
-                  My Account
-                </Typography>
-              </ListItemText>
+              <ListItemButton onClick={() => navigate('/login')}>
+                <ListItemText>
+                  <Typography lineHeight={2} variant="capton2">
+                    Login
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+              <ListItemButton onClick={() => navigate('/signup')}>
+                <ListItemText>
+                  <Typography lineHeight={2} variant="capton2">
+                    SignUp
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
+              <ListItemButton onClick={() => navigate('/myaccount')}>
+                <ListItemText>
+                  <Typography lineHeight={2} variant="capton2">
+                    My Account
+                  </Typography>
+                </ListItemText>
+              </ListItemButton>
             </List>
           </Grid>
           <Grid item md={6} lg={4}>
