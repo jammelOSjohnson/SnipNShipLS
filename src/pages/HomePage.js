d960dce7fe15935450df54b506f4815b64f12db0
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2023-05-20 12:47:44 -0500

    change home and 404 tab titles

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,13 @@
+import { Helmet } from 'react-helmet-async';
 import Banner from '../components/banner';
 import ContactUs from '../components/contactus';
 
 export default function HomePage() {
   return (
     <>
+      <Helmet>
+        <title> Home | Snip & Ship </title>
+      </Helmet>
       <Banner />
       <ContactUs />
     </>
