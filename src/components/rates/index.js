4d6948563867dfd9651746933b90aa435c4580e2
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2023-05-28 14:57:56 -0500

    add rates table

--- src/components/rates/index.js
+++ src/components/rates/index.js
@@ -1,5 +1,17 @@
 import { useState } from 'react';
-import { Box, Grid, Tab, Typography } from '@mui/material';
+import {
+  Box,
+  Grid,
+  Paper,
+  Tab,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import { RatesContainer, RatesTab } from '../../styles/rates';
 import { Colors } from '../../theme/palette';
@@ -9,6 +21,36 @@ export default function Rates() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  function createData(pound, cost) {
+    return { pound, cost };
+  }
+
+  const rows = [
+    createData('1 lb', '$560 JMD'),
+    createData('2 lb', '$560 JMD'),
+    createData('3 lb', '$1,000 JMD'),
+    createData('4 lb', '$1,250 JMD'),
+    createData('5 lb', '$1,550 JMD'),
+    createData('6 lb', '$1,850 JMD'),
+    createData('7 lb', '$2,150 JMD'),
+    createData('8 lb', '$2,450 JMD'),
+    createData('9 lb', '$2,750 JMD'),
+    createData('10 lb', '$3,050 JMD'),
+  ];
+
+  const rows2 = [
+    createData('1 lb', '$560 JMD'),
+    createData('2 lb', '$560 JMD'),
+    createData('3 lb', '$1,000 JMD'),
+    createData('4 lb', '$1,250 JMD'),
+    createData('5 lb', '$1,550 JMD'),
+    createData('6 lb', '$1,850 JMD'),
+    createData('7 lb', '$2,150 JMD'),
+    createData('8 lb', '$2,450 JMD'),
+    createData('9 lb', '$2,750 JMD'),
+    createData('10 lb', '$3,050 JMD'),
+  ];
 
   return (
     <>
@@ -23,18 +65,67 @@ export default function Rates() {
             <Typography variant="h2">Our Rates</Typography>
           </Grid>
         </Grid>
-        <RatesTab>
-          <TabContext value={value}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-              <TabList onChange={handleChange}>
-                <Tab label="Air Freight" value="1" />
-                <Tab label="Sea Freight" value="2" />
-              </TabList>
-            </Box>
-            <TabPanel value="1">Air Freight</TabPanel>
-            <TabPanel value="2">Sea Freight</TabPanel>
-          </TabContext>
-        </RatesTab>
+        <Grid container>
+          <Grid item xs={1} md={3} />
+          <Grid item xs={10} md={6}>
+            <TableContainer component={Paper} sx={{ backgroundColor: 'transparent' }}>
+              <Table sx={{ minWidth: 320 }} aria-label="airfreight table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell sx={{ fontWeight: 900, color: Colors.primary, fontSize: '1.5em' }}>Weight</TableCell>
+                    <TableCell sx={{ fontWeight: 900, color: Colors.primary, fontSize: '1.5em' }}>Cost</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow key={row.pound} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                      <TableCell component="th" scope="row" sx={{ color: Colors.white }}>
+                        {row.pound}
+                      </TableCell>
+                      <TableCell sx={{ color: Colors.white }}>{row.cost}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            {/* <RatesTab>
+              <TabContext value={value}>
+                <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                  <TabList onChange={handleChange}>
+                    <Tab label="Air Freight" value="1" />
+                    <Tab label="Sea Freight" value="2" />
+                  </TabList>
+                </Box>
+                <TabPanel value="1">
+                  
+                </TabPanel>
+                <TabPanel value="2">
+                <TableContainer component={Paper}>
+                    <Table sx={{ minWidth: 320 }} aria-label="airfreight table">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Weight</TableCell>
+                          <TableCell>Cost</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        {rows2.map((row) => (
+                          <TableRow key={row.pound} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell component="th" scope="row">
+                              {row.pound}
+                            </TableCell>
+                            <TableCell>{row.cost}</TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </TabPanel>
+              </TabContext>
+            </RatesTab> */}
+          </Grid>
+          <Grid item xs={1} md={3} />
+        </Grid>
       </RatesContainer>
     </>
   );
