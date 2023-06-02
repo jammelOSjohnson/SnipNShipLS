3b57ec9cd811bc007ad0edb63eb9f85d66c2ce87
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2023-06-02 15:39:29 -0500

    configuring google signup

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 // @mui
@@ -10,6 +11,8 @@ import Logo from '../components/logo';
 import Iconify from '../components/iconify';
 // sections
 import { SignUpForm } from '../sections/auth/signup';
+// context
+import { useGeneral } from '../context/general';
 
 // ----------------------------------------------------------------------
 
@@ -46,6 +49,55 @@ const StyledContent = styled('div')(({ theme }) => ({
 
 export default function SignupPage() {
   const mdUp = useResponsive('up', 'md');
+  const { value } = useGeneral();
+  const { signup, gLogin, fLogin, tLogin, fetchUserInfoForSignUp, fetchUserInfo } = value;
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleGoogleSubmit = async function handleGoogleSubmit(event) {
+    event.preventDefault();
+    // prevents default form refresh
+    // console.log("I am inside Google Submit fuction");
+    try {
+      setError('');
+      setLoading(true);
+      await gLogin(value).then(async (res1) => {
+        if (res1 != null) {
+          await fetchUserDetails(res1).then((res) => {
+            if (res) {
+              // console.log("About to close signup modal.");
+              setLoading(false);
+              // setLoggedIn(true);
+              // console.log("About to navigate to dashboard.");
+              // history.push("/Dashboard");
+            } else {
+              setError('Unable to signup at this time');
+              setLoading(false);
+            }
+          });
+        } else {
+          setError('Unable to signup at this time.');
+          setLoading(false);
+        }
+      });
+    } catch {
+      setError('Failed to signup');
+      setLoading(false);
+    }
+  };
+
+  const fetchUserDetails = async function fetchUserDetails(payload) {
+    // console.log("Is current user null");
+    // console.log(value);
+    if (payload.currentUser !== null && payload.currentUser !== undefined) {
+      if (payload.currentUser.uid !== null && payload.currentUser.uid !== undefined) {
+        // console.log("Fetching user info");
+        // console.log(state);
+        return fetchUserInfo(payload.currentUser.uid, payload);
+      }
+    }
+    return false;
+  };
 
   return (
     <>
@@ -85,7 +137,7 @@ export default function SignupPage() {
             </Typography>
 
             <Stack direction="row" spacing={2}>
-              <Button fullWidth size="large" color="inherit" variant="outlined">
+              <Button fullWidth size="large" color="inherit" variant="outlined" onClick={() => handleGoogleSubmit}>
                 <Iconify icon="eva:google-fill" color="#DF3E30" width={22} height={22} />
               </Button>
 
