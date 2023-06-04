55b27680ce62188a1227e55f4d1f4a3f777f1015
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2023-06-04 16:24:29 -0500

    fix how the context provider was being passed

--- src/App.js
+++ src/App.js
@@ -15,23 +15,29 @@ import Scroll from './components/scroll';
 
 // ----------------------------------------------------------------------
 
-export default function App() {
+const App = function App() {
   return (
     <HelmetProvider>
       <BrowserRouter>
         <ThemeProvider>
-          <GeneralProvider>
-            <UIProvider>
-              <Scroll showBelow={250} />
-              <ScrollToTop />
-              <StyledChart />
-              <AppDrawer />
-              <SearchBox />
-              <Router />
-            </UIProvider>
-          </GeneralProvider>
+          <UIProvider>
+            <Scroll showBelow={250} />
+            <ScrollToTop />
+            <StyledChart />
+            <AppDrawer />
+            <SearchBox />
+            <Router />
+          </UIProvider>
         </ThemeProvider>
       </BrowserRouter>
     </HelmetProvider>
+  );
+};
+
+export default function () {
+  return (
+    <GeneralProvider>
+      <App />
+    </GeneralProvider>
   );
 }
