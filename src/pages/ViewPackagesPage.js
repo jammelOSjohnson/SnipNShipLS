4c4c275c1095df89ad17a2dbf5295e01bdbcb489
Jammel Johnson <jammel.johnson@myguardiangroup.com>
2023-06-18 17:50:15 -0500

    add dependencies to useEffect

--- src/pages/ViewPackagesPage.js
+++ src/pages/ViewPackagesPage.js
@@ -141,7 +141,7 @@ export default function ViewPackagesPage() {
       });
       setRows(tempRows);
     }
-  }, [packages]);
+  }, [packages, fetchPackages, currentUser, rows.length, value]);
 
   return (
     <>
